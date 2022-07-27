f4b9d8d6e6a4ec7575fb58ef16059fab5ba79c51
Srishti Singh <srishtisingh102@gmail.com>
2022-07-27 21:31:31 +0000

    updating search

--- src/main/webapp/scriptsearch.js
+++ src/main/webapp/scriptsearch.js
@@ -1,14 +1,20 @@
-var table = $('#example').DataTable({
-    columns: [{
-        "title": "Food Name",
-        "data": "Food Name"
-    }, {
-        "title": "Ingredients",
-        "data": "Ingredients"
-    }]
-});
-$("#populateTable").click(function() {
-    console.log($("#csvImport").val());
-    console.log($.csv.toObjects($("#csvImport").val()));
-    table.rows.add($.csv.toObjects($("#csvImport").val())).draw();
-});
+google.charts.load('current', {'packages':['table']});
+google.charts.setOnLoadCallback(drawTable);
+
+/** Fetches foods and ingredients data and uses it to create a dataTable. */
+function drawTable() {
+  fetch('/foods-data').then(response => response.json())
+  .then((foodsList) => {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Food Name');
+    data.addColumn('string', 'Ingredients');
+    
+    Object.keys(foodsList).forEach((name) => {
+      data.addRow([name, foodsList[name]]);
+    });
+
+    var table = new google.visualization.Table(document.getElementById('table_div'));
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});    
+  });
+}
+
